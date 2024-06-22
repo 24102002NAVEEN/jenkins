2e2dac64e762fd3b3aa71cc1c95a5dc86a158076
24102002NAVEEN <23203015@srcas.ac.in>
2024-06-22 11:54:29 +0530

    Create hellow.java

---
+++ hellow.java
@@ -0,0 +1,7 @@
+public class Hello{
+public Static void main(String[] args){
+system.out.println("welcome");
+system.out.println("to");
+system.out.println("deveops");
+system.out.println("class");
+}
